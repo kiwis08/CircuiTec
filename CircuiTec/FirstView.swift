22cd6bf4648c578a2cd1b7ec270143539d52524a
FerRomo09 <102379163+FerRomo09@users.noreply.github.com>
2024-03-21 16:00:44 -0600

    feat: improve main screen

--- CircuiTec/FirstView.swift
+++ CircuiTec/FirstView.swift
@@ -7,6 +7,8 @@
 
 import SwiftUI
 import MapKit
+import UIKit
+import CoreLocation
 
 struct FirstView: View {
     
@@ -15,28 +17,34 @@ struct FirstView: View {
             Image(imageName)
                 .resizable()
                 .aspectRatio(contentMode: .fill)
-                .frame(width: 80, height: 60)
+                .frame(width: 70, height: 40)
                 .padding(.top)
             
             //Card
             VStack(alignment: .leading, spacing: 3) {
                 Text(title)
                     .font(.headline)
-                    .multilineTextAlignment(.leading)
 
                 Text(subtitle)
                     .font(.footnote)
                     .foregroundColor(.gray)
-                    .multilineTextAlignment(.leading)
+                    .padding(.bottom)
 
             }
+            .padding(.horizontal, 5)
+
+            
         }
-        .frame(width: 110, height: 160)
+        .frame(width: 110, height: 120)
         .background(Color.white)
         .clipShape(RoundedRectangle(cornerRadius: 10.0))
         .shadow(radius: 6)
-        .padding(.horizontal, 3)
+        .padding(.horizontal, 2)
+        .padding(.vertical)
     }
+    
+    
+    
 
     
     
@@ -45,31 +53,87 @@ struct FirstView: View {
         NavigationStack {
             VStack {
                 
-                // Mapa
-                Map()
-                    .mapStyle(.standard)
-                    .frame(width:350, height: 450)
-                    .clipShape(RoundedRectangle(cornerRadius: 10.0))
-                    .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
-                    .padding(.vertical, 10)
-                
+                //Barra de busqueda
                 HStack {
-                    Text("Parada sugerida")
+                    Text("Buscar destino")
+                        .padding(.horizontal)
+                    
+                    Spacer()
+                    
+                    Button(action: {
+                        // Acción que quieres que ocurra cuando se presiona el botón
+                        print("Botón presionado")
+                    }) {
+                        Image(systemName: "magnifyingglass")
+                            .padding(.horizontal)
+                    }
+                }
+                .frame(width: 350, height: 40)
+                .background(Color.white)
+                .clipShape(RoundedRectangle(cornerRadius: 10.0))
+                .shadow(radius: 10)
+                .padding(.horizontal, 2)
+                .padding(.vertical)
+            
+                
+                           
+                           
+                // Mapa
+                VStack {
+                    HStack {
+                        Text("Parada más cercana")
+                            .font(.headline)
+                        .multilineTextAlignment(.leading)
+                        Spacer()
+                    }
+                    .padding(.horizontal, 20)
+                    
+                    Map()
+                        .mapStyle(.standard)
+                        .frame(width:350, height: 200)
+                        .clipShape(RoundedRectangle(cornerRadius: 10.0))
+                        .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+                    .padding(.bottom, 10)
+                }
+                
+                
+                //Favoritos
+                HStack {
+                    Text("Favoritos")
                         .font(.headline)
                         .multilineTextAlignment(.leading)
                     Spacer()
+                    Button(action: {
+                                // Acción que quieres que ocurra cuando se presiona el botón
+                                print("Botón presionado")
+                            }) {
+                                Image(systemName: "plus.circle")
+                                    .padding(.horizontal)
+                            }
                 }
                 .padding(.horizontal, 20)
+                .padding(.bottom, 20)
+                .padding(.top, 20)
 
                     
                 
                 //Cards
-                HStack{
-                    card(imageName: "garza sada", title: "Parada", subtitle: "Parada sugerida")
-                    card(imageName: "mapa", title: "Cómo llegar", subtitle: "Camina a tu parada")
-                    card(imageName: "mapa", title: "Hora de llegada", subtitle: "7 minutos")
-            
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack {
+                        card(imageName: "revolucion", title: "Parada", subtitle: "Nuevo Sur")
+                        card(imageName: "casa", title: "Casa", subtitle: "Lazaro cárdenas #2525")
+                        card(imageName: "tienda", title: "Walmart las Torres", subtitle: "Eugenio Garza Sada 6110")
+                        card(imageName: "garza sada", title: "IMSS Clínica 33", subtitle: "Ruta Garza Sada")
+                        card(imageName: "hospitales", title: "Oxxo Río Pánuco", subtitle: "Ruta Hospitales y Escuelas")
+                        card(imageName: "mapa", title: "Casa Dani", subtitle: "Junco de la Vega #2443")
+                    }
                 }
+                .frame(height: 100)
+                .transition(.move(edge: .bottom))
+                .padding(.horizontal, 10)
+                
+                Spacer()
+            
                 
             }
             .navigationTitle("¡Hola!")
